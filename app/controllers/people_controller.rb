10750942e200eba17aa06b257a8f2c1a2742f719
Alexandra Hanley <alexandrahanley@gmail.com>
2015-02-26 11:06:25 -0700

    user can edit and update a person with error alerts

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,13 +12,36 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
 
+  def edit
+    @person = Person.find(params[:id])
+  end
+
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to @person, notice: 'Person was successfully updated.'
+    else
+      @sign_in_error = "Invalid Naming Convention"
+      render :edit
+    end
+  end
+
   def create
     @person = Person.new(params.require(:person).permit(:first_name, :last_name, :title))
     if @person.save
       redirect_to root_path, notice: "Person was created!"
     else
+      @sign_in_error = "Invalid Naming Convention"
       render :new
     end
   end
+
+private
+
+    def person_params
+      params.require(:person).permit(:first_name, :last_name, :title)
+    end
+
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,11 @@
 class Person < ActiveRecord::Base
+validates :title && :last_name, presence: true || :first_name && :last_name, presence: true
+
 
   def full_name
     "#{title} #{first_name} #{last_name}"
   end
+
 
 
 end
